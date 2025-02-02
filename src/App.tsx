5b20251ded85c6a92ae7c14b172611674dddf665
Alek240825 <iglesiag1908@gmail.com>
2025-02-01 19:29:58 -0500

    cambio de variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { useState, useEffect } from "react"
 import './App.css'
-import Home from './pages/Home'
+import Home from './pages/home'
 import Game from './pages/game'
 
 function App() {
